c2b54f465b5090312333438240430ba1c333f305
friggito <ornella.friggit@gmail.com>
2019-06-01 18:29:19 -0400

    feat (insights): fixed formula to get sum of all expenses, reformatted

--- client/components/insights.js
+++ client/components/insights.js
@@ -1,49 +1,50 @@
 import React from 'react'
 import {OverviewCard} from '.'
-import { connect } from 'react-redux';
+import {connect} from 'react-redux'
 
 /**
  * COMPONENT
-*/
+ */
 
-function     sum (array, key){
-    let sum = 0
-    for (let i = 0; i < array.length; i++){
-        if (key){
-            sum += Number(array[i][key])
-        } else{
-            sum += Number(array[i])
-        }
+function sum(array, key) {
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    if (key) {
+      sum += Number(array[i][key])
+    } else {
+      sum += Number(array[i])
     }
-    return sum
+  }
+  return sum
 }
 export const Insights = props => {
-    // constructor(props) {
-    //     super(props)
-    //     this.sum = this.sum.bind(this)
-    //   }
-
-    // render(){
-        const {expenses, debts, monthlySalary} = props
-        let sumExpenses = sum(Object.values(expenses))
-        let sumDebts = sum(debts, "amount")
-        return (
-          <div className="col l9">
-              <h1>WHoA</h1>
-              <div id="overview-row">
-                  <OverviewCard color={"red"} number={sumExpenses} title={"Monthly expenses"}/>
-                  <OverviewCard color={"green"} number={monthlySalary} title={"Monthly salary"}/>
-                  <OverviewCard color={"blue"} number={sumDebts} title={"Total debts"}/>
-              </div>
-          </div>
-        )
-    // }
+  const {expenses, debts, monthlySalary} = props
+  let sumExpenses = Math.floor(1.15 * sum(Object.values(expenses)))
+  let sumDebts = sum(debts, 'amount')
+  return (
+    <div className="col l9">
+      <h1>WHoA</h1>
+      <div id="overview-row">
+        <OverviewCard
+          color={'red'}
+          number={sumExpenses}
+          title={'Monthly expenses'}
+        />
+        <OverviewCard
+          color={'green'}
+          number={monthlySalary}
+          title={'Monthly salary'}
+        />
+        <OverviewCard color={'blue'} number={sumDebts} title={'Total debts'} />
+      </div>
+    </div>
+  )
 }
 
-const mapProps = (props) => ({
-    expenses: props.expenses,
-    monthlySalary: Math.floor(props.salary / 12),
-    debts: props.debts.debtsList
+const mapProps = props => ({
+  expenses: props.expenses,
+  monthlySalary: Math.floor(props.salary / 12),
+  debts: props.debts.debtsList
 })
 
 export default connect(mapProps)(Insights)
