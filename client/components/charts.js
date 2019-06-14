ab09231cd0be8618af64c8da75d7079e7f25a8ec
friggito <ornella.friggit@gmail.com>
2019-06-14 14:09:11 -0400

    Refactored to move data transformation functions to the parent component

--- client/components/charts.js
+++ client/components/charts.js
@@ -5,14 +5,60 @@ import {FinancialChart} from '.'
  * COMPONENT
  */
 
+class Charts extends React.Component {
+  constructor() {
+    super()
+    this.payOneMonth = this.payOneMonth.bind(this)
+    this.getFinancials = this.getFinancials.bind(this)
+  }
 
-export const Charts = props => {
-  const {months, monthlyExpenses, debts, monthlySalary, savingsGoal} = props
-  return (
-    <div className="row">
-      <FinancialChart months={months} debts={debts} monthlyExpenses={monthlyExpenses} monthlySalary={monthlySalary} />
-    </div>
-  )
+  payOneMonth(debts, monthIdx) {
+    let paid = 0
+    debts.forEach(debt => {
+      if (debt.timeline > monthIdx) {
+        paid += debt.amount / debt.timeline
+      }
+    })
+    return paid
+  }
+
+  getFinancials(months, debts, monthlyExpenses, monthlySalary) {
+    let debtsPaid = []
+    let savings = []
+    let expenses = []
+    for (let i = 0; i < months; i++) {
+      let debtPayment = this.payOneMonth(debts, i)
+      debtsPaid.push(debtPayment)
+      savings.push(monthlySalary - monthlyExpenses - debtPayment)
+      expenses.push(monthlyExpenses)
+    }
+    return [
+      {title: 'expenses', data: expenses, color: 'orange'},
+      {title: 'debts paid', data: debtsPaid, color: '#ff4081'},
+      {title: 'savings', data: savings, color: '#d4e157'}
+    ]
+  }
+
+  render() {
+    const {
+      months,
+      monthlyExpenses,
+      debts,
+      monthlySalary,
+      savingsGoal
+    } = this.props
+    const finances = this.getFinancials(
+      months,
+      debts,
+      monthlyExpenses,
+      monthlySalary
+    )
+    return (
+      <div className="row">
+        <FinancialChart financialData={finances} monthlySalary={monthlySalary} />
+      </div>
+    )
+  }
 }
 
 export default Charts
--- client/components/financial-chart.js
+++ client/components/financial-chart.js
@@ -1,56 +1,16 @@
 import React from 'react'
 import {VictoryStack, VictoryChart, VictoryBar, VictoryAxis} from 'victory'
 
-class FinancialChart extends React.Component {
-  constructor() {
-    super()
-    this.payOneMonth = this.payOneMonth.bind(this)
-    this.getFinancials = this.getFinancials.bind(this)
-  }
-
-  payOneMonth(debts, monthIdx) {
-    let paid = 0
-    debts.forEach(debt => {
-      if (debt.timeline > monthIdx) {
-        paid += debt.amount / debt.timeline
-      }
-    })
-    return paid
-  }
-
-  getFinancials(months, debts, monthlyExpenses, monthlySalary) {
-    let debtsPaid = []
-    let savings = []
-    let expenses = []
-    for (let i = 0; i < months; i++) {
-      let debtPayment = this.payOneMonth(debts, i)
-      debtsPaid.push(debtPayment)
-      savings.push(monthlySalary - monthlyExpenses - debtPayment)
-      expenses.push(monthlyExpenses)
-    }
-    return [
-      {title: 'expenses', data: expenses, color: 'orange'},
-      {title: 'debts paid', data: debtsPaid, color: '#ff4081'},
-      {title: 'savings', data: savings, color: 'lime'}
-    ]
-  }
-
-  render() {
-    const {months, debts, monthlyExpenses, monthlySalary} = this.props
-    const finances = this.getFinancials(
-      months,
-      debts,
-      monthlyExpenses,
-      monthlySalary
-    )
+function FinancialChart (props) {
+    const {financialData = [], monthlySalary} = props
     return (
       <div className="card-panel" id="financial-chart">
         <VictoryChart>
           <VictoryStack
             vertical
-            style={{data: {strokeWidth: 1.5, fillOpacity: 0.4, width: 6}}}
+            style={{data: {strokeWidth: 1, fillOpacity: 0.8, width: 6}}}
           >
-            {finances.map(category => (
+            {financialData.map(category => (
               <VictoryBar
                 key={category.title}
                 data={category.data}
@@ -58,7 +18,7 @@ class FinancialChart extends React.Component {
               />
             ))}
             <VictoryAxis
-              tickValues={finances[0]['data'].map((item, idx) => idx)}
+              tickValues={financialData[0]['data'].map((item, idx) => idx)}
               tickFormat={t => t + 1}
             />
             <VictoryAxis dependentAxis tickValues={[0, monthlySalary]} />
@@ -66,7 +26,6 @@ class FinancialChart extends React.Component {
         </VictoryChart>
       </div>
     )
-  }
 }
 
 export default FinancialChart
